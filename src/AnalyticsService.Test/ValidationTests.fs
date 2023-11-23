0c4ddca399775b9ff5e9a02d5d6411316efffc0e
Michal Moudrý <moudry.michal@live.com>
2023-11-23 15:56:51 +0100

    Update ValidationTests.fs

--- src/AnalyticsService.Test/ValidationTests.fs
+++ src/AnalyticsService.Test/ValidationTests.fs
@@ -1,6 +1,7 @@
 module AnalyticsService.Test.ValidationTests
 
 open System
+open System.Text.Json
 open NUnit.Framework
 open AnalyticsService.Transport.Validation
 open AnalyticsService.Transport.Contracts
@@ -17,3 +18,12 @@ let TestBatchStatRequestValidation () =
         WorkflowId = Guid.NewGuid() 
     }
     Assert.IsTrue(validator.Validate(request).IsValid)
+
+[<TestCase("{\"batch_id\":\"121536e8-17d5-4673-9fde-e327e7830206\",\"end_date\":\"2023-11-20 11:31:43.832218\",\"number_of_documents\":1,\"start_date\":\"2023-11-20 11:31:43.832215\",\"status\":1,\"workflow_id\":\"b82696de-b495-4ea1-8c19-c5eda902ce74\"}")>]
+let TestActualBatchStatBody(body: string) =
+    let deserializedBody =
+        try
+            Some(JsonSerializer.Deserialize<BatchStatRequest>(body))
+        with
+            | :? JsonException as err -> printfn $"Exception! %s{err.Message} "; None
+    Assert.That(deserializedBody.IsSome, Is.True)
