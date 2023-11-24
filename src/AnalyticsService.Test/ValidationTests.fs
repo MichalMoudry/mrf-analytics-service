099687ad5d8aebf8b4d241aa1a9e0bfc8d756e5b
Michal Moudrý <moudry.michal@live.com>
2023-11-24 15:25:39 +0100

    Prod refactoring + added CI/CD pipeline (#15)
    * Disabled Swagger UI for prod + updated Dapr controller
    
    * Removed unnecessary using
    
    * Update ValidationTests.fs
    
    * Updated validation test
    
    * Updated validation test
    
    * Added CI/CD pipeline definition

--- src/AnalyticsService.Test/ValidationTests.fs
+++ src/AnalyticsService.Test/ValidationTests.fs
@@ -1,6 +1,7 @@
 module AnalyticsService.Test.ValidationTests
 
 open System
+open System.Text.Json
 open NUnit.Framework
 open AnalyticsService.Transport.Validation
 open AnalyticsService.Transport.Contracts
@@ -17,3 +18,13 @@ let TestBatchStatRequestValidation () =
         WorkflowId = Guid.NewGuid() 
     }
     Assert.IsTrue(validator.Validate(request).IsValid)
+
+/// A test covering a deserialization of a message from MQ.
+[<TestCase("{\"batch_id\":\"121536e8-17d5-4673-9fde-e327e7830206\",\"end_date\":\"2023-11-24T13:29:08.322434\",\"number_of_documents\":1,\"start_date\":\"2023-11-20T11:31:43\",\"status\":1,\"workflow_id\":\"b82696de-b495-4ea1-8c19-c5eda902ce74\"}")>]
+let TestActualBatchStatBody(body: string) =
+    let deserializedBody =
+        try
+            Some(JsonSerializer.Deserialize<BatchStatRequest>(body))
+        with
+            | :? JsonException as err -> printfn $"Exception! %s{err.Message}\n {err}"; None
+    Assert.That(deserializedBody.IsSome, Is.True)
