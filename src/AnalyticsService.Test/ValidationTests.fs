c56c7b62143910e3750e26e94875bc8571eb9c5b
Michal Moudrý <moudry.michal@live.com>
2023-11-24 14:18:11 +0100

    Updated validation test

--- src/AnalyticsService.Test/ValidationTests.fs
+++ src/AnalyticsService.Test/ValidationTests.fs
@@ -19,11 +19,12 @@ let TestBatchStatRequestValidation () =
     }
     Assert.IsTrue(validator.Validate(request).IsValid)
 
-[<TestCase("{\"batch_id\":\"121536e8-17d5-4673-9fde-e327e7830206\",\"end_date\":\"2023-11-20 11:31:43.832218\",\"number_of_documents\":1,\"start_date\":\"2023-11-20 11:31:43.832215\",\"status\":1,\"workflow_id\":\"b82696de-b495-4ea1-8c19-c5eda902ce74\"}")>]
+/// A test covering a deserialization of a message from MQ.
+[<TestCase("{\"batch_id\":\"121536e8-17d5-4673-9fde-e327e7830206\",\"end_date\":\"2023-11-20T11:31:43\",\"number_of_documents\":1,\"start_date\":\"2023-11-20T11:31:43\",\"status\":1,\"workflow_id\":\"b82696de-b495-4ea1-8c19-c5eda902ce74\"}")>]
 let TestActualBatchStatBody(body: string) =
     let deserializedBody =
         try
             Some(JsonSerializer.Deserialize<BatchStatRequest>(body))
         with
-            | :? JsonException as err -> printfn $"Exception! %s{err.Message} "; None
+            | :? JsonException as err -> printfn $"Exception! %s{err.Message}\n {err}"; None
     Assert.That(deserializedBody.IsSome, Is.True)
