5329d79b24d2bde264498c76d859f3ed41e5c291
Michal Moudrý <moudry.michal@live.com>
2023-11-24 14:31:34 +0100

    Updated validation test

--- src/AnalyticsService.Test/ValidationTests.fs
+++ src/AnalyticsService.Test/ValidationTests.fs
@@ -20,7 +20,7 @@ let TestBatchStatRequestValidation () =
     Assert.IsTrue(validator.Validate(request).IsValid)
 
 /// A test covering a deserialization of a message from MQ.
-[<TestCase("{\"batch_id\":\"121536e8-17d5-4673-9fde-e327e7830206\",\"end_date\":\"2023-11-20T11:31:43\",\"number_of_documents\":1,\"start_date\":\"2023-11-20T11:31:43\",\"status\":1,\"workflow_id\":\"b82696de-b495-4ea1-8c19-c5eda902ce74\"}")>]
+[<TestCase("{\"batch_id\":\"121536e8-17d5-4673-9fde-e327e7830206\",\"end_date\":\"2023-11-24T13:29:08.322434\",\"number_of_documents\":1,\"start_date\":\"2023-11-20T11:31:43\",\"status\":1,\"workflow_id\":\"b82696de-b495-4ea1-8c19-c5eda902ce74\"}")>]
 let TestActualBatchStatBody(body: string) =
     let deserializedBody =
         try
