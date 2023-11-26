a5e88dacfb7e68114bd2691771a1c50721f4e5e8
Michal Moudrý <moudry.michal@live.com>
2023-11-26 10:43:58 +0100

    Added background service

---
+++ src/AnalyticsService.BgRunner/Program.fs
@@ -0,0 +1,24 @@
+namespace AnalyticsService.BgRunner
+
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+
+module Program =
+    let createHostBuilder args =
+        (*
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices(fun hostContext services ->
+                services.AddHostedService<Worker>() |> ignore)
+        *)
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices(
+                fun _ services -> (
+                    services.AddHostedService<Worker>() |> ignore
+                )
+            )
+
+    [<EntryPoint>]
+    let main args =
+        createHostBuilder(args).Build().Run()
+
+        0 // exit code
