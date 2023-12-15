83e78dbd65ebf54b3d9073b7672afaae2854a8ce
Michal Moudrý <moudry.michal@live.com>
2023-12-15 15:52:58 +0100

    Update DlqRepository.fs

--- src/AnalyticsService.Database/Repositories/DlqRepository.fs
+++ src/AnalyticsService.Database/Repositories/DlqRepository.fs
@@ -2,6 +2,24 @@
 [<Sealed>]
 module AnalyticsService.Database.Repositories.DlqRepository
 
+open AnalyticsService.Database.Model
 open Dapper.FSharp.PostgreSQL
+open System
+open System.Data
 
-let private dlqTable = table'<Dead> "DLQ"
+let private dlqTable = table'<DeadTopic> "DLQ"
+
+let newDlqItem deadTopic (conn: IDbConnection) =
+    task {
+        conn.Open()
+        use transaction = conn.BeginTransaction()
+        
+        transaction.Commit()
+    }
+
+/// Method for selecting all items from dead letter queue.
+let getDlqItems<'T> (conn: IDbConnection) =
+    select {
+        for _ in dlqTable do
+            selectAll
+    } |> conn.SelectAsync<'T>
